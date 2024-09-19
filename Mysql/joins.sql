8a8eee5ec44ca82da1fbc5e6d284ec78e2eeb926
Irving LLerena Bejarano <irvingllerena@gmail.com>
2024-09-18 20:17:12 -0500

    ejercicios complementarios de joins

--- Mysql/joins.sql
+++ Mysql/joins.sql
@@ -79,3 +79,51 @@ LEFT JOIN ventas v
 ON e.id = v.empleado_id
 GROUP BY d.id;
 
+--Ejercicios Complementarios
+
+--Muestra el nombre y apellido de los empleados que pertenecen al departamento de "Recursos Humanos" y han realizado más de 5 ventas.
+SELECT
+e.nombre as nombre_empleado,
+e.apellido as apellido_empleado,
+COUNT(v.id) as cantidad_ventas
+FROM empleados e
+INNER JOIN ventas v
+ON e.id = v.empleado_id
+WHERE e.departamento_id = 2
+GROUP BY e.id
+HAVING COUNT(v.id) > 5;
+
+--Muestra el nombre y apellido de todos los empleados junto con la cantidad total de ventas que han realizado, incluso si no han realizado ventas.
+SELECT
+e.nombre as nombre_empleado,
+e.apellido as apellido_empleado,
+COUNT(v.id) as cantidad_ventas
+FROM empleados e
+LEFT JOIN ventas v
+ON e.id = v.empleado_id
+GROUP BY e.id;
+
+--Encuentra el empleado más joven de cada departamento y muestra el nombre del departamento junto con el nombre y la edad del empleado más joven.
+SELECT
+d.nombre as nombre_departamento,
+e.nombre as nombre_empleado,
+e.apellido as apellido_empleado,
+e.edad as edad_empleado
+FROM departamentos d
+INNER JOIN empleados e
+ON d.id = e.departamento_id
+WHERE e.edad = (SELECT MIN(edad) FROM empleados WHERE departamento_id = e.departamento_id);
+
+--Calcula el volumen de productos vendidos por cada producto (por ejemplo, menos de 5 como "bajo", entre 5 y 10 como "medio", y más de 10 como "alto") y muestra la categoría de volumen junto con la cantidad y el nombre del producto.
+SELECT
+p.nombre as nombre_producto,
+CASE
+    WHEN SUM(v.cantidad) < 5 THEN 'Bajo'
+    WHEN SUM(v.cantidad) BETWEEN 5 AND 10 THEN 'Medio'
+    ELSE 'Alto'
+END as volumen,
+SUM(v.cantidad) as cantidad_vendida
+FROM productos p
+LEFT JOIN ventas v
+ON p.id = v.producto_id
+GROUP BY p.id;
